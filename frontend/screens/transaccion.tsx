1af88556f5a515bd5e9e6278848f25c4522b4c0e
CesarRD-98 <dedioscesar12@gmail.com>
2025-06-05 20:43:38 -0600

    commit inicial

---
+++ frontend/screens/transaccion.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Transaccion() {
+  return (
+    <View>
+      <Text>Transaccion</Text>
+    </View>
+  )
+}
